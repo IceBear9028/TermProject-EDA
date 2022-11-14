8ea485e9e0be4b493b084b139429333da57e1dd5
IceBear9028 <tlatms9028@gmail.com>
2022-11-15 03:50:17 +0900

    0.1 commit
    먼저 callback 함수 연결하지 않았을 때 시트가 여러개 생기는 코드는 완성
    그런데 생긴 시트(인스턴스) 에 대해서는 콜백함수가 작동하지 않음
    
    ++ 문법이 잘못되었는지 확인해볼것

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import plotly
+import plotly.express as px
 import plotly.graph_objects as go
 import math
 import pandas as pd
 import numpy as np
-from dash_player import DashPlayer
-from dash import Dash, dcc, html, Input, Output,ctx, State
+from dash import Dash, dcc, html, Input, Output,ctx, State, MATCH
 from flask import Flask, Response, request
 import dash_bootstrap_components as dbc
 
@@ -77,14 +76,8 @@ app.layout = html.Div(
 )
 # 5. 레이아웃에 callback 함수 생성
 # 0. 인스턴스 생성시 필요한 변수들
-global sheet_num, sheet_input_list_1,sheet_input_list_2,sheet_input_list_3, sheet_output_list
+global sheet_num
 sheet_num = 1
-sheet_input_list_1 = []
-sheet_input_list_2 = []
-sheet_input_list_3 = []
-sheet_output_list = []
-
-
 
 # a. 변수들 정보들 
 @app.callback(
@@ -95,29 +88,32 @@ def info_window(n):
     if n %2 == 1:
         return info_content
 
-# b. sheet 를 추가하는 버튼
+# b. sheet 를 추가하는 버튼 기능 활성화
 @app.callback(
     Output(sheet_container,'children'),
     Input(sheet_plus_btn, 'n_clicks'),
     State(sheet_container, 'children')
 )
 def sheet_plus(n,container):
-    global sheet_num, sheet_input_list, sheet_output_list
     sheet_instance = SheetPage()
-    globals()["{0}_sheet".format(sheet_num)] = sheet_instance.create_layout(create_sheet_index(sheet_num))
-    sheet_num += 1
-    container.append(sheet_instance.create_layout(create_sheet_index(sheet_num)))
+    container.append(sheet_instance.create_layout(n, df.columns))
     return container
 
-def create_sheet_index(sheet_num):
-    drop_id_1 = '{0}_sheet_ddn1'.format(sheet_num)
-    drop_id_2 = '{0}_sheet_ddn2'.format(sheet_num)
-    drop_id_3 = '{0}_sheet_ddn1'.format(sheet_num)
-    graph_id = '{0}_sheet_graph'.format(sheet_num)
-    drop_data_2 = df.columns
-    drop_data_3 = df.columns
-    return drop_id_1, drop_id_2, drop_id_3, graph_id, drop_data_2, drop_data_3
-    
+# graph_category = [i['value'] for i in graph_options]
+# 각 Sheet 끼리 dropdown,component_property component_id
+# @app.callback(
+#     Output(component_id ={'type' :'graph', 'index' : MATCH}, component_property ='figure'),
+#     [Input(component_id = {'type' : 'dynamic-x1', 'index' : MATCH}, component_property = 'value'),
+#     Input(component_id = {'type' : 'dynamic-x2', 'index' : MATCH}, component_property = 'value'),
+#     Input(component_id ={'type' : 'graph-change', 'index' : MATCH}, component_property ='value')]
+# )
+# def indatance_graph_update(x1, x2, choice_graph ):
+#     if choice_graph == 'scatter':
+#         fig = px.scatter(df, x = x1, y = x2)
+#         return fig
+#     # elif choice_graph == 'box':
+#     #     fig = px.box(df, x = x1, y = x2)
+#     #     return fig
 
 # 6. 최종 생성한 서버 run
 if __name__ == '__main__':
--- src/sheetpage.py
+++ src/sheetpage.py
@@ -1,57 +1,69 @@
 from dash import dcc, html
-from dash import Dash, dcc, html, Input, Output
+
+# graph_option = [
+#             {'label' : 'Scatter', 'value' : 'scatter'},
+#             {'label' : 'Box', 'value' : 'box'}
+#         ]
+
 
 # 클래스 형태로 페이지 기능을 구현한다.
-graph_settings = [
-    {'label' : 'Scatter', 'value' : 'scatter'},
-    {'label' : 'Box', 'value' : 'box'}
-]
-
-
 class SheetPage:
     def __init__(self):
         # containr 의 div className
         # -> 여러개의 Sheet가 생길때, container의 스타일을 한번에 수정하기 용이함
+        self.graph_option = [
+            {'label' : 'Scatter', 'value' : 'scatter'},
+            {'label' : 'Box', 'value' : 'box'}
+        ]
 
-        # dropdown의 id
-        self.dropdown_id_1 = None
-        self.dropdown_id_2 = None
-        self.dropdown_id_3 = None
-
-        # 그래프 id
-        self.graph_id = None
-        
-        # dropdown 데이터
-        self.dd_data_id2 = None
-        self.dd_data_id3 = None
-
-    
-    def create_layout(self, tuple):
-        self.dropdown_id_1 = tuple[0]
-        self.dropdown_id_2 = tuple[1]
-        self.dropdown_id_3 = tuple[2]
-        self.graph_id = tuple[3]
-        self.dd_data_id2 = tuple[4]
-        self.dd_data_id3 = tuple[5]
-
+    def create_layout(self, index, dropdown_option):
+        # index : 동적으로 인스턴스 생성시 인스턴스끼리 구분하기 위한 번호
+        # dropdown_option : dropdown에 들어갈 값
         return html.Div(
                 className = 'instanceSheetContainer',
                 children = [
                     dcc.Dropdown(
-                        id = self.dropdown_id_1,
-                        options = graph_settings
+                        id = {
+                            'type' : 'graph-change',
+                            'index' : index
+                        },
+                        options = self.graph_option, 
+                        style = {
+                            'width' : '800px'
+                        },
+                        value = dropdown_option[0]
                     ),
-                    dcc.Graph(id = self.graph_id),
+                    dcc.Graph(
+                        id = {
+                            'type' : 'graph',
+                            'index' : index
+                        },
+                        figure = {}
+                    ),
                     html.Div(
                         className = 'xyValueChoiceContainer',
                         children = [
                             dcc.Dropdown(
-                                id = self.dropdown_id_2,
-                                options = self.dd_data_id2
+                                id = {
+                                    'type' : 'dynamic-x1',
+                                    'index' : index
+                                    },
+                                options = [{'label' : item, 'value' : item} for item in dropdown_option],
+                                style = {
+                                    'width' : '430px'
+                                },
+                                value = dropdown_option[0]
                             ),
                             dcc.Dropdown(
-                                id = self.dropdown_id_3,
-                                options = self.dd_data_id3
+                                id = {
+                                    'type':'dynamic-x2',
+                                    'index': index
+                                },
+                                options = [{'label' : item, 'value' : item} for item in dropdown_option],
+                                style = {
+                                    'width' : '430px'
+                                },
+                                value = dropdown_option[0]
                             )
                         ]
                     )
